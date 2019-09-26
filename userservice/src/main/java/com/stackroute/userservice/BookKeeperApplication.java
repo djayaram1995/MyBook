b1b9d2a6b675f46a0eecc3f7f3b196d569fc09bd
djayaram1995 <jayaramdeenu@gmail.com>
2019-09-26 06:21:53 +0530

    commit all

--- userservice/src/main/java/com/stackroute/userservice/BookKeeperApplication.java
+++ userservice/src/main/java/com/stackroute/userservice/BookKeeperApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class BookKeeperApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(BookKeeperApplication.class, args);
 	}
 
 }
-
